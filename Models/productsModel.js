6a82ef3618ce972628c6d68e01ae1fcf57e76191
ayaatea <aaatea03@gmail.com>
2023-12-16 22:58:51 +0200

    2th update 4 schemas

--- Models/productsModel.js
+++ Models/productsModel.js
@@ -1,31 +1,34 @@
-import {model,Schema} from 'mongoose';
+import { model, Schema } from 'mongoose';
 const productSchema = new schema(
-{
-    name: {
-    type: String,
-    required: [true, "Product Name is Required"],
+    {
+        name: {
+            type: String,
+            trim: true,
+            minlength: [2, "min length must be 2 char"],
+            maxlength: [50, "max length must be 50 char"],
+            required: [true, "Product Name is Required"],
+        },
+        quantity: {
+            type: Number,
+            default: 0,
+            trim: true,
+            required: [true, "Product Quantity is Requried"],
+        },
+        price: {
+            type: Number,
+            trim: true,
+            required: [true, "Price is Required"],
+        },
+        sold: {
+            type: Number,
+            default: 0,
+        },
+        category: {
+            type: Schema.ObjectId,
+            ref: "categoryModel",
+            required: [true, "Category Must Be Required"],
+        },
     },
-    quantity: {
-    type: Number,
-    default: 0,
-    trim: true,
-    required: [true, "Product Quantity is Requried"],
-    },
-    price: {
-    type: Number,
-    trim: true,
-    required: [true, "Price is Required"],
-    },
-    sold: {
-    type: Number,
-    default: 0,
-    },
-    category: {
-    type: Schema.ObjectId,
-    ref: "categoryModel",
-    required: [true, "Category Must Be Required"],
-    },
-},
-{ timestamps: true }
+    { timestamps: true }
 );
 export default model("productModel", productSchema);
