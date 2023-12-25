197512da329cb0ac5577693e3b4b9aa826a0ee2a
boodyshlby55 <boodyshlby55@gmail.com>
2023-12-25 21:36:08 +0200

    end of create bill

--- controllers/bills.js
+++ controllers/bills.js
@@ -1,11 +1,52 @@
-import billModel from '../Models/billsModel.js'
+import expressAsyncHandler from 'express-async-handler';
 import { deleteOne, updateOne, getAll, createOne, findOne } from '../controllers/refactorHandler.js'
+import billModel from '../Models/billsModel.js'
+import productsModel from '../Models/productsModel.js';
 
 
 
 
 //  create Bills post 
-export const createBills = createOne(billModel);
+export const createBills = expressAsyncHandler(async (req, res) => {
+
+    const productQuantityMap = req.body.productQuantityMap || {};
+    const products = req.body.products
+    const productDetailsPromises = products.map(async (productData) => {
+        const productId = productData.product;
+        const requestedQuantity = productQuantityMap[productId] || 0;
+
+        // Check if the product exists in DB
+        const product = await productsModel.findById(productId);
+        if (!product) {
+            return Promise.reject(new Error(`Product not found for ID: ${productId}`));
+        }
+
+        // Check for the quantity of products
+        if (requestedQuantity <= 0 || requestedQuantity > product.quantity) {
+            return Promise.reject(new Error(`Invalid quantity for product: ${product.name}`));
+        }
+
+        // Update the product quantity in the database
+        await productsModel.findByIdAndUpdate(productId, {
+            $inc: {
+                quantity: -requestedQuantity,
+                sold: requestedQuantity
+            }
+        });
+
+        return {
+            product: productData.product,
+            productQuantity: requestedQuantity,
+            totalPrice: product.price * requestedQuantity,
+        };
+    });
+    const productDetails = await Promise.all(productDetailsPromises);
+
+    req.body.products = productDetails; // Update req.body.products with detailed information
+
+    const bill = await billModel.create(req.body)
+    res.status(200).json({ data: bill })
+});
 
 // view list Bills get 
 export const AllBills = getAll(billModel, "billModel")
--- utils/validation/billsValidator.js
+++ utils/validation/billsValidator.js
@@ -15,38 +15,8 @@ export const createBillsValidator = [
         .optional()
         .isMobilePhone("ar-EG").withMessage("InValid Phone Number only accept EG Number "),
     check("products")
-        .notEmpty().withMessage("products is required")
-        .isMongoId().withMessage("Invalid Id")
-        .custom(async (productIds, { req }) => {
-            // Check if products exists in DB
-            const products = await productModel.find({ _id: { $exists: true, $in: productIds } });
-            if (products.length < 1 || products.length != productIds.length) { return Promise.reject(new Error("No products for this Id")); }
-
-            // Check for the quantity of products
-            const productQuantityMap = req.body.productQuantityMap || {};
-            for (const product of products) {
-                const productId = product._id.toString();
-                const requestedQuantity = productQuantityMap[productId] || 0;
-                if (requestedQuantity <= 0 || requestedQuantity > product.quantity) {
-                    return Promise.reject(new Error(`Invalid quantity for product: ${product.name}`));
-                }
-            }
-            // Update product quantities in the database
-            await Promise.all(products.map(async (product) => {
-                const productId = product._id.toString();
-                const requestedQuantity = productQuantityMap[productId] || 0;
-
-                // Update the product quantity in the database
-                await productModel.findByIdAndUpdate(productId, {
-                    $inc: {
-                        quantity: -requestedQuantity,
-                        sold: requestedQuantity
-                    }
-                });
-            }));
-
-            return true;
-        }),
+        .notEmpty().withMessage("Products are required")
+        .isArray().withMessage("Products must be an array"),
     validatorMiddleware,
 ];
 
@@ -70,12 +40,7 @@ export const updateBillValidator = [
         .isMobilePhone("ar-EG").withMessage("InValid Phone Number only accept EG Number "),
     check("products")
         .optional()
-        .isMongoId().withMessage("Invalid Id")
-        .custom((productIds) =>
-            productModel.find({ _id: { $exists: true, $in: productIds } }).then((products) => {
-                if (products.length < 1 || products.length != productIds.length) { return Promise.reject(new Error("No products for this Id")); }
-            })
-        ),
+        .isArray().withMessage("Products must be an array"),
     validatorMiddleware,
 ];
 
