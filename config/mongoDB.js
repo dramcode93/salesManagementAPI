3745fe5da6d8c5b9510a053fd1b50c764fefc13b
boodyshlby55 <boodyshlby55@gmail.com>
2023-12-31 19:48:10 +0200

    update DB

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -1,6 +1,16 @@
+import expressAsyncHandler from "express-async-handler";
 import mongoose from "mongoose";
 
-export const DBConnection = () => {
-    mongoose.connect(process.env.mongoDB)
-        .then(console.log("Database Connected"))
-};
+// export const DBConnection = () => {
+//     mongoose.connect(process.env.mongoDB)
+//         .then(console.log("Database Connected"))
+// };
+
+export const DBConnection = expressAsyncHandler(async () => {
+    try {
+        await mongoose.connect(process.env.mongoDB);
+        console.log('Database Connected');
+    } catch (err) {
+        console.error('Error connecting to the database: ', err.message);
+    }
+})
