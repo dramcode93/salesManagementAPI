de0f4090c4c10d7aedbacdfb1e3cbef0103138f9
boodyshlby55 <boodyshlby55@gmail.com>
2024-01-30 23:08:41 +0200

    resetPassword validation

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { forgetPassword, login, resetPassword, verifyResetPasswordCode } from "../controllers/auth.js";
-import { loginValidator } from "../utils/validation/authValidator.js";
+import { loginValidator, resetPasswordValidator } from "../utils/validation/authValidator.js";
 
 const router = new Router();
 router.route('/login').post(loginValidator, login);
 router.route('/forgetPassword').post(forgetPassword);
 router.route('/verifyResetPasswordCode').post(verifyResetPasswordCode);
-router.route('/resetPassword').put(resetPassword);
+router.route('/resetPassword').put(resetPasswordValidator, resetPassword);
 export default router;
